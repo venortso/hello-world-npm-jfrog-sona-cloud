82bc695c72f8fd30a1be4e0789fbfa9a88527147
LuongVitEnvorso <vito.luong@envorso.com>
2023-09-13 13:34:02 -0700

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export CI_BUILD_NAME="github-action-venortso"
-export CI_BUILD_NUM="1.0.0"
+export CI_BUILD_NAME="venortso-npm-challenge"
+export CI_BUILD_NUM="1.0.3"
 
 # Please modify to match the artifacts to be uploaded for your project
 jf npm publish --build-name=$CI_BUILD_NAME --build-number=$CI_BUILD_NUM --flat=false 
