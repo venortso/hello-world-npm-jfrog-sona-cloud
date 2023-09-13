217997caaf05cc4e16cf1cd7d6ef91f5b85dfbe1
LuongVitEnvorso <vito.luong@envorso.com>
2023-09-12 18:35:12 -0700

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,10 +4,10 @@ export CI_BUILD_NAME="github-action-venortso"
 export CI_BUILD_NUM="1.0.0"
 
 # Please modify to match the artifacts to be uploaded for your project
-jf npm publish --build-name=$CI_BUILD_NAME --build-number=$CI_BUILD_NUM --flat=false --server-id=jf1 
+jf npm publish --build-name=$CI_BUILD_NAME --build-number=$CI_BUILD_NUM --flat=false 
 
 # read env vars
 jf rt bce $CI_BUILD_NAME $CI_BUILD_NUM
 
 # Publish build info
-jf rt bp $CI_BUILD_NAME $CI_BUILD_NUM --server-id=jf1
+jf rt bp $CI_BUILD_NAME $CI_BUILD_NUM
